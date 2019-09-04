9d27a57dee24a06b2b551992456a9884ebae925e
HappyFreeman <valeriibadirov@mail.ru>
2019-09-05 00:37:21 +0300

    Лабораторная работа 1.

---
+++ Task1.4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	float a, b;
+	cin >> a >> b;
+	cout << "Sum = " << a + b << endl;
+	cout << "Difference = " << a - b << endl;
+	cout << "Multiplication = " << a * b << endl;
+	cout << "Division = " << a / b << endl;
+}
