5725ffd8210bd3ccfc4596f9972773c2650a76ad
HappyFreeman <valeriibadirov@mail.ru>
2019-09-05 21:20:52 +0300

    update

--- LaboratoryWork#2/Task2.4.cpp
+++ LaboratoryWork#2/Task2.4.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-	float x1, x2, y1, y2, Sid1, Sid2;
+	float x1, x2, y1, y2, Side1, Side2;
 	cout << "Enter x1 and y1: " << endl;
 	cin >> x1 >> y1;
 	cout << "Enter x2 and y2: " << endl;
 	cin >> x2 >> y2;
 	//Diagonal = sqrt( pow(x2 - x1, 2) + pow(y2 - y1, 2) );
-	Sid1 = abs(y2 - y1);       //sqrt( pow(y2 - y1, 2) ) иксы сократились в формуле
-	Sid2 = abs(x2 - x1);       //возможно можно было легче решить)
-	cout << "Perimeter = " << 2 * (Sid1 + Sid2) << endl;
-	cout << "Area = " << Sid1 * Sid2 << endl;  // не знаю правильно или нет)
+	Side1 = abs(y2 - y1);       //sqrt( pow(y2 - y1, 2) ) иксы сократились в формуле
+	Side2 = abs(x2 - x1);       //возможно можно было легче решить)
+	cout << "Perimeter = " << 2 * (Side1 + Side2) << endl;
+	cout << "Area = " << Side1 * Side2 << endl;  // не знаю правильно или нет)
 }
--- LaboratoryWork#2/Task2.5.cpp
+++ LaboratoryWork#2/Task2.5.cpp
@@ -5,18 +5,18 @@ using namespace std;
 
 int main()
 {
-	float x1, x2, x3, y1, y2, y3, Sid1, Sid2, Sid3, p, S;
+	float x1, x2, x3, y1, y2, y3, Side1, Side2, Side3, p, S;
 	cout << "Enter x1 and y1: " << endl;
 	cin >> x1 >> y1;
 	cout << "Enter x2 and y2: " << endl;
 	cin >> x2 >> y2;
 	cout << "Enter x3 and y3: " << endl;
 	cin >> x3 >> y3;
-	Sid1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
-	Sid2 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
-	Sid3 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
-	p = (Sid1 + Sid2 + Sid3) / 2;  // полупериметр
-	cout << "Perimeter = " << Sid1 + Sid2 + Sid3 << endl;
-	S = p * ((p - Sid1) * (p - Sid2) * (p - Sid3)); // формула Герона
+	Side1 = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+	Side2 = sqrt(pow(x3 - x1, 2) + pow(y3 - y1, 2));
+	Side3 = sqrt(pow(x3 - x2, 2) + pow(y3 - y2, 2));
+	p = (Side1 + Side2 + Side3) / 2;  // полупериметр
+	cout << "Perimeter = " << Side1 + Side2 + Side3 << endl;
+	S = p * ((p - Side1) * (p - Side2) * (p - Side3)); // формула Герона
 	cout << "Area = " << S << endl;
 }
