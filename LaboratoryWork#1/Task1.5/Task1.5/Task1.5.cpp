f9013cf1f79fd1caf27ce18ec8f707e30d53f71d
HappyFreeman <valeriibadirov@mail.ru>
2019-09-05 20:49:56 +0300

    Check

---
+++ LaboratoryWork#1/Task1.5/Task1.5/Task1.5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	float a, b;
+	cin >> a >> b;
+	a = abs(a);
+	b = abs(b);
+	cout << "Sum = " << a + b << endl;
+	cout << "Difference = " << a - b << endl;
+	cout << "Multiplication = " << a * b << endl;
+	cout << "Division = " << a / b << endl;
+}
---
+++ LaboratoryWork#2/Task2.2/Task2.2/Task2.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <math.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	float A, B, C, AC, BC, Sum;
+	cout << "Enter A, B, C : " << endl;
+	cin >> A >> B >> C;
+	AC = abs(A - C);
+	BC = abs(B - C);
+	Sum = AC + BC;
+	cout << "Length AC = " << AC << endl;
+	cout << "Length BC = " << BC << endl;
+	cout << "Sum AC and BC = " << Sum << endl;
+}
