cb1dce54a8fe11b414cc8e1fab378190db7c655c
HappyFreeman <valeriibadirov@mail.ru>
2019-09-25 11:01:56 +0300

    f

--- LaboratoryWork#3/Task3.2.cpp
+++ LaboratoryWork#3/Task3.2.cpp
@@ -4,12 +4,13 @@ using namespace std;
 
 int main()
 {
-	int A, B, C;
+	int A, B, C, q;
 	cout << "Enter A, B, C: " << endl;
 	cin >> A >> B >> C;
+	q = A;
 	A = B;
 	B = C;
-	C = A;
+	C = q;
 	cout << "A = " << A << endl;
 	cout << "B = " << B << endl;
 	cout << "C = " << C << endl; // Я правильно понял задание?
